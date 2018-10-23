1969244804f5212d38fcde9509ebbc06ff91128d
Andrei Bratkovski <andrei.a.bratkovski@gmail.com>
2018-10-23 17:52:11 -0400

    Tested anagram

---
+++ CCI-4E/Arrays-and-Strings/anagram.py
@@ -0,0 +1,28 @@
+"""
+1.4 Write a method to decide if two strings are anagrams
+or not
+"""
+import unittest
+
+
+def anagram(str1, str2):
+    return sorted(str1) == sorted(str2)
+
+
+class Test(unittest.TestCase):
+    true_test_data = [("listen", "silent"), ("hithere", "hithere"),
+                      ("", ""), ("None", "None")]
+    false_test_data = [("hithere", "bro"), ("thi s", "shou ld fail"),
+                       ("nogood", "")]
+
+    def test_anagram(self):
+        for item in self.true_test_data:
+            result = anagram(item[0], item[1])
+            self.assertTrue(result)
+        for item in self.false_test_data:
+            result = anagram(item[0], item[1])
+            self.assertFalse(result)
+
+
+if __name__ == "__main__":
+    unittest.main()
