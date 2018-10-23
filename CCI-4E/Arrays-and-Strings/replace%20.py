e308f7e5e1f467b1d6e3e7d5d56ebd5dcf7c17b7
Andrei Bratkovski <andrei.a.bratkovski@gmail.com>
2018-10-23 18:14:18 -0400

    urlify

--- CCI-4E/Arrays-and-Strings/replace%20.py
+++ CCI-4E/Arrays-and-Strings/replace%20.py
@@ -11,6 +11,20 @@ def urlify(input_str):
     return input_str.replace(" ", "%20")
 
 
+#
+# The non-LOL approach.
+#
+def primitive_urlify(input_str, str_len):
+    list_input_str = list(input_str)
+    new_str = ""
+    for item in list_input_str:
+        if item == ' ':
+            new_str += "%20"
+        else:
+            new_str += item
+    return new_str
+
+
 class Test(unittest.TestCase):
     false_test_data = [("", "s"), ("  ", "%20 2")]
     true_test_data = [("hi there", "hi%20there"),
@@ -23,6 +37,15 @@ class Test(unittest.TestCase):
         for item in self.true_test_data:
             result = urlify(item[0])
             self.assertEqual(result, item[1])
+
+    def test_primitive_urlify(self):
+        def test_urlify(self):
+            for item in self.false_test_data:
+                result = primitive_urlify(item[0], len(item[0]))
+                self.assertNotEqual(result, item[1])
+            for item in self.true_test_data:
+                result = urlify(item[0], len(item[0]))
+                self.assertEqual(result, item[1])
 
 
 if __name__ == "__main__":
